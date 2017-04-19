cd5a21bd5d3f01e24c77a9f0710c9cfa5c06dca8
Wojciech Kocjan <wojciech@kocjan.org>
2017-04-19 13:24:47 +0200

    Added license

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const child_process = require('child_process');
 let ipAddress = '(unknown)';
 let helloName = '(unknown)';
 
+// this may require net-tools system package or equivalent to work
 try {
   const ifconfigOutput = child_process.execSync('ifconfig').toString();
   ipAddress = ifconfigOutput.match(/inet\s+addr:\s*(.*?)\s/)[1].trim();
