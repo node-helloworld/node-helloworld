a3c374bf2d6923cf54bf89c64b5e2a00f28c5aba
Wojciech Kocjan <wojciech@kocjan.org>
2017-04-19 11:52:56 +0200

    Initial import

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+const child_process = require('child_process');
+
+let ipAddress = '(unknown)';
+let helloName = '(unknown)';
+
+try {
+  const ifconfigOutput = child_process.execSync('ifconfig').toString();
+  ipAddress = ifconfigOutput.match(/inet\s+addr:\s*(.*?)\s/)[1].trim();
+} catch (e) {
+  console.error(e);
+}
+
+try {
+  helloName = fs.readFileSync('./name.txt').toString().trim();
+} catch (e) {
+  console.error(e);
+}
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain;'})
+  res.end(`Hello ${helloName}!
+My IP address is ${ipAddress}
+`);
+}).listen(8080);
