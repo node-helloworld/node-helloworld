f34c3ccbe9c4a0f19c4cb38d65caa968ed45b4ff
Wojciech Kocjan <wojciech@kocjan.org>
2017-05-23 18:11:12 +0200

    Adds static file support and HTML welcome page

--- app.js
+++ app.js
@@ -21,11 +21,46 @@ try {
   console.error(e);
 }
 
+function serveStaticFile(req, res, filename, contentType) {
+  fs.readFile(filename, function(error, content) {
+    if (error) {
+      res.statusCode = 404;
+      res.end('File not found');
+      return;
+    }
+    res.setHeader('Content-type', contentType);
+    res.end(content);
+  });
+}
+
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain;'})
-  res.end(`Hello ${helloName}!
+  if (req.url === '/favicon.ico') {
+    serveStaticFile(req, res, './images/favicon.ico', 'image/x-icon');
+  } else if (req.url === '/images/helloworld.png') {
+    serveStaticFile(req, res, '.' + req.url, 'image/png');
+  } else if (req.url === '/index.html') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`<html>
+<head>
+<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+</head>
+<body>
+<p>
+Hello ${helloName}!
+</p>
+<p>
+My IP address is ${ipAddress}
+</p>
+<img src="/images/helloworld.png" width="128" height="128" border="0" />
+</body>
+</html>
+`);
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(`Hello ${helloName}!
 My IP address is ${ipAddress}
 `);
+  }
 }).listen(8080);
 
 // helper
